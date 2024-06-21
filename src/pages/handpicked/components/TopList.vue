2c66f099336323fd5ae366b48f3f5d15cf9db2d0
Leet <76251617+skyline523@users.noreply.github.com>
2024-06-21 17:53:42 +0800

    chore: 榜单精选ui

--- src/pages/handpicked/components/TopList.vue
+++ src/pages/handpicked/components/TopList.vue
@@ -10,8 +10,8 @@ const { data: topList } = useRequest(getTopListSum, {
 </script>
 
 <template>
-  <div>
-    <div flex="~ justify-start" px-6>
+  <div px-6>
+    <div flex="~ justify-start">
       <div flex="~ items-center">
         <p font-extrabold class="text-hover">
           榜单精选
@@ -19,21 +19,28 @@ const { data: topList } = useRequest(getTopListSum, {
         <div i-solar-alt-arrow-right-linear />
       </div>
     </div>
-    <div v-if="topList">
-      <div v-for="item in topList.list" :key="item.id">
-        <div>
-          <span>{{ item.name }}</span>
-          <span>{{ item.updateFrequency }}</span>
+    <div v-if="topList" grid="~ cols-2 gap-6">
+      <div
+        v-for="item in topList.list"
+        :key="item.id"
+        rounded-lg bg-white p-6 shadow-md
+      >
+        <div flex="~ items-center justify-between">
+          <span text="xl" font-bold>{{ item.name }}</span>
+          <span text="sm gray-400">{{ item.updateFrequency }}</span>
         </div>
-        <div>
-          <div>
-            <img :src="item.coverImgUrl">
+        <div flex="~ items-center">
+          <div h="78px" w="78px">
+            <img h="full" w="full" rounded-md object-contain :src="item.coverImgUrl">
           </div>
           <div>
-            <div v-for="(track, index) in item.tracks" :key="track.first">
-              <span>{{ index + 1 }}</span>
-              <span>{{ track.first }}</span>
-              <span> - {{ track.second }}</span>
+            <div
+              v-for="(track, index) in item.tracks"
+              :key="track.first"
+
+              flex="~"
+            >
+              <span line-clamp-1>{{ index + 1 }}<span> {{ track.first }} - {{ track.second }}</span></span>
             </div>
           </div>
         </div>
