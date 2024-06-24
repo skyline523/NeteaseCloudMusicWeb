35a4a0a498f85dd28c585015ccc1f6f2b06098b0
Leet <76251617+skyline523@users.noreply.github.com>
2024-06-24 14:42:32 +0800

    chore: 删除unocss字体配置, 添加shortcuts

--- uno.config.ts
+++ uno.config.ts
@@ -22,6 +22,7 @@ export default defineConfig({
   shortcuts: [
     ['btn', 'px-4 py-1 rounded inline-block bg-teal-700 text-white cursor-pointer !outline-none hover:bg-teal-800 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
     ['icon-btn', 'inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-teal-600'],
+    ['secondary-text', 'text-gray-500/80'],
   ],
   presets: [
     presetUno(),
@@ -30,13 +31,7 @@ export default defineConfig({
       scale: 1.2,
     }),
     presetTypography(),
-    presetWebFonts({
-      fonts: {
-        sans: 'DM Sans',
-        serif: 'DM Serif Display',
-        mono: 'DM Mono',
-      },
-    }),
+    presetWebFonts(),
   ],
   transformers: [
     transformerDirectives(),
