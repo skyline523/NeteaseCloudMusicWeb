6ac15c06124500211aeaf1ee430cb773b87f9b5a
Leet <76251617+skyline523@users.noreply.github.com>
2024-06-24 14:44:18 +0800

    feat: 新增公共组件LeArtistText

---
+++ src/components/LeArtistText/index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import type { Artist } from '~/apis/artist/types'
+
+defineOptions({
+  name: 'LeArtistText',
+})
+
+defineProps({
+  artists: {
+    type: Array as PropType<Artist[]>,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <span v-for="(item, index) in artists" :key="item.id">
+    <span>{{ item.name }}</span>
+    <span v-if="index < artists.length - 1">/</span>
+  </span>
+</template>
