02fe50d3e83623454a5a18de6f0586a9f3612a87
Leet <76251617+skyline523@users.noreply.github.com>
2024-07-08 17:00:46 +0800

    chore: 修改html meta和icon

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,15 @@
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
 useHead({
-  title: 'Vitesse',
+  title: '网易云音乐',
   meta: [
     {
+      name: 'keywords',
+      content: '网易云音乐,音乐,播放器,网易,下载,播放,DJ,免费,明星,精选,歌单,识别音乐,收藏,分享音乐,音乐互动,高音质,320K,音乐社交',
+    },
+    {
       name: 'description',
-      content: 'Opinionated Vite Starter Template',
+      content: '网易云音乐是一款专注于发现与分享的音乐产品,依托专业音乐人、DJ、好友推荐及社交功能,为用户打造全新的音乐生活。',
     },
     {
       name: 'theme-color',
@@ -17,8 +21,8 @@ useHead({
   link: [
     {
       rel: 'icon',
-      type: 'image/svg+xml',
-      href: '/favicon.svg',
+      type: 'image/png',
+      href: '/favicon.png',
     },
   ],
   script: [
