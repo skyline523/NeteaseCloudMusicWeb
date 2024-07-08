9224dd461fd80ada36d632264b16a5417281d63d
Leet <76251617+skyline523@users.noreply.github.com>
2024-07-08 16:38:16 +0800

    chore: 删除perfect-scrollbar相关文件

--- src/modules/scrollbar.ts
+++
@@ -1,7 +0,0 @@
-import { PerfectScrollbarPlugin } from 'vue3-perfect-scrollbar'
-import type { UserModule } from '~/types'
-
-export const install: UserModule = ({ isClient, app }) => {
-  if (isClient)
-    app.use(PerfectScrollbarPlugin)
-}
